0cc745e877a84e805869785fad819aff8c53d628
Soyamprabha <mallick.soyam@gmail.com>
2023-03-29 23:07:24 +0530

    seller

--- public/js/seller.js
+++ public/js/seller.js
@@ -1,3 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCq3Q5P2c2NJcZMtD7JJl59h7kWKthS79w",
+  authDomain: "evive-8a537.firebaseapp.com",
+  databaseURL: "https://evive-8a537-default-rtdb.firebaseio.com",
+  projectId: "evive-8a537",
+  storageBucket: "evive-8a537.appspot.com",
+  messagingSenderId: "1007208283634",
+  appId: "1:1007208283634:web:9759ec2552d4eb44663f6b",
+  measurementId: "G-ZNQG7W6V7N"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
 window.onload = () => {
     let user = JSON.parse(sessionStorage.user || null);
     
